0fc4cbd436240e2f592e27253d8b3a68a8236a57
Gust <60011212221@msu.ac.th>
2020-01-04 12:32:20 +0700

    aicommit

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php
-for($i= 1 ;$i<=5;$i++){
-    echo '<i class=\"fas fa-star\" style=\"font-size: 50px;color: gold\"></i>';
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <meta charset="UTF-8">
+    <title>test</title>
+</head>
+<body>
+<h1>Ai</h1>
+    <?php
+    for($i= 1 ;$i<=5;$i++) {
+        echo '<i class="fas fa-star" style="font-size: 50px;color: gold"></i>';
+    }
+    ?>
+</body>
+</html>
+
