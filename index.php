278164fa1998df12268c733b5f731fc23a0ddaa6
Gust <60011212221@msu.ac.th>
2020-01-04 12:45:06 +0700

    aicommit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
 <h1>Ai</h1>
     <?php
-    for($i= 1 ;$i<=5;$i++) {
+    for($i= 1 ;$i<=10;$i++) {
         echo '<i class="fas fa-star" style="font-size: 50px;color: gold"></i>';
     }
     ?>
