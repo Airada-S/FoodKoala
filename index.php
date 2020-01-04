9fae7e1a6e14b7f688152313f94b9a26d29fc79d
Gust <60011212221@msu.ac.th>
2020-01-04 19:33:49 +0700

    new

--- index.php
+++ index.php
@@ -26,37 +26,60 @@
         while($row = $result->fetch_assoc()) {
 ?>
 
-<div class="card border-danger m-5 " style="max-width: 18rem;">
-<!--    <div class="card-header bg-transparent border-danger">Header</div>-->
-    <div class="card-body text-danger">
-        <img class="card-img-top"
-             src="<?php echo $row["seller_img"]?>"
-             alt="Card image cap">
-        <h5 class="card-title"><?php echo $row["seller_name"]?></h5>
-        <p class="card-text">
-            <?php
-                for($i= 1 ;$i<=4;$i++) {
-                    echo '<i class="fas fa-star" style="font-size: 20px;color: gold"></i>';
-                }
-                $sql2 = "SELECT DISTINCT product_type FROM product WHERE seller_id = ".$row["seller_id"];
-                $result2 = $conn->query($sql2);
-            ?>
-            <br>ประ :
-            <?php
-                if ($result2->num_rows > 0) {
-                // output data of each row
-                while($row2 = $result2->fetch_assoc()) {
-                    echo $row2["product_type"].",";
-                }}
-                ?>
-            <br>
-            เปิด <?php echo $row["seller_time"]?><br>
-            ที่อยู่ :<br>
-            <?php echo $row["seller_address"]?>
-        </p>
-    </div>
-<!--    <div class="card-footer bg-transparent border-danger">Footer</div>-->
-</div>
+            <div class="card border-danger m-5 " style="max-width: 18rem;">
+                <!--    <div class="card-header bg-transparent border-danger">Header</div>-->
+                <div class="card-body">
+                    <img class="card-img-top"
+                         src="<?php echo $row["seller_img"]; ?>"
+                         alt="Card image cap">
+                    <h5 class="card-title text-danger"><?php echo $row["seller_name"] ?></h5>
+                    <p class="card-text">
+                        <?php
+                        $sql3 = "SELECT `reviews_start` FROM `reviews` WHERE `seller_id` = '".$row["seller_id"]."'";
+                        $result3 = $conn->query($sql3);
+                        $sum = 0;
+
+                        if ($result3->num_rows > 0) {
+                            $n = 0;
+                            // output data of each row
+                            while($row3 = $result3->fetch_assoc()) {
+                                $sum = $sum+$row3["reviews_start"];
+                                $n++;
+                            }
+                        }
+                        $strat = floor($sum/$n);
+                        for($i= 1 ;$i<=5;$i++) {
+                            if($strat >= $i){
+                                echo '<i class="fas fa-star" style="font-size: 20px;color: gold"></i>';
+                            }else{
+                                echo '<i class="far fa-star" style="font-size: 20px;color: gold"></i>';
+                            }
+
+                        }
+                        $sql2 = "SELECT DISTINCT `product_type` FROM product WHERE `seller_id` = '".$row["seller_id"]."'";
+                        $result2 = $conn->query($sql2);
+                        ?>
+                        <br>ประเภท :
+                        <?php
+                        if ($result2->num_rows > 0) {
+                            $i = 1;
+                            // output data of each row
+                            while($row2 = $result2->fetch_assoc()) {
+                                if( $i%2 == 0){
+                                    echo " , ";
+                                }
+                                echo $row2["product_type"];
+                                $i++;
+                            }}
+                        ?>
+                        <br>
+                        เปิด <?php echo $row["seller_time"]?><br>
+                        ที่อยู่ :<br>
+                        <?php echo $row["seller_address"]?>
+                    </p>
+                </div>
+                <!--    <div class="card-footer bg-transparent border-danger">Footer</div>-->
+            </div>
 <?php
         }
     } else {
