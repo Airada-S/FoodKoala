682fcdd9c26a61d886d0066c3ce818ef9477a4ad
Natthawut chipimol <natthawut_game2541@hotmail.com>
2020-01-14 12:51:23 +0700

    game commit

--- AddOderProduct.php
+++ AddOderProduct.php
@@ -23,7 +23,9 @@ include 'header.php';
                         </li>
                     </ul>
                     <h5 class="card-title">เวลาส่งอาหาร</h5>
-                    <input type="time" style="width: 45%;" >
+                    <select id="date">
+                        <option></option>
+                    </select>
                     <input type="time" style="width: 45%;">
                     <h5 class="card-title mt-5">ที่อยู่สำหรับจัดส่ง</h5>
                     <div class="card" style="width: 50%; border-color: #e06c6c;">
@@ -74,5 +76,24 @@ include 'header.php';
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script>
+    let tomorrow = new Date();
+    const x = document.getElementById("date");
+    let date;
+    var months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+    const option = document.createElement("option");
+    date = tomorrow.getDate()+"/"+months[tomorrow.getMonth()]+"/"+tomorrow.getFullYear();
+    option.text = date;
+    x.add(option);
+    for (let i = 1; i < 5; i++) {
+        const option = document.createElement("option");
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        date = tomorrow.getDate()+"/"+months[tomorrow.getMonth()]+"/"+tomorrow.getFullYear();
+        option.text = date;
+        x.add(option);
+    }
+    document.getElementById("date").selectedIndex = "1";
+</script>
 </body>
 </html>
