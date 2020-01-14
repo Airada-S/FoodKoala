d012fe994cc552a6fb15e789fecb889bc815333e
NAMO_OK <60011212208@msu.ac.th>
2020-01-14 15:45:27 +0700

    na

---
+++ manage.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>สมัครสมาชิก</title>
+    <link rel="stylesheet" type="text/css" href="loginCSS.css" title="style1">
+
+</head>
+<body>
+<?php
+include 'header.php';
+require("SetSessionStatus.php");
+?>
+
+<div class="login-page">
+    <div class="form tab-content">
+            <form class="login-form"  method='POST'>
+                <img src="img/logo.png" width="250" height="250" class="d-inline-block align-top" alt="">
+                <button style="margin-top: 15px" >จัดการข้อมูลพนักงาน</button>
+                <button style="margin-top: 15px" >อนุมัติร้านค้า</button>
+            </form>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
