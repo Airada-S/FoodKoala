2c266c00035b1628245b6326d18f631acc666d41
กมลทิพย์ เทศทอง <60011212001@o365.msu.ac.th>
2020-01-14 16:04:46 +0700

    New Clone ShopManage

--- ShopManage.php
+++ ShopManage.php
@@ -7,20 +7,108 @@
 <body>
 <?php
 include 'header.php';
-require("SetSessionStatus.php");
-?>
+require_once './ConnectDatabase.php';
 
-<div style="margin: 30px; border: 1px solid #c26f6f; width: 95%;">
-    <div>
-        <table>
-            <tr>
-                <td rowspan="2"><img src=""></td>
-                <td><strong>Username : </strong></td>
-            </tr>
-            <tr>
-                <td><strong>e-mail : </strong></td>
-            </tr>
-        </table>
+$conn = new ConnectDB();
+
+?>
+<div style="padding-top: 40px; padding-left: 150px; padding-right: 150px;">
+    <div style="margin: 30px; border: 1px solid #c26f6f; width: 95%; border-radius: 5px;">
+        <div style="margin: 20px;">
+            <table style="margin-left: 100px; margin-right: 100px; width: 90%;">
+                <tr>
+                    <td rowspan="4" STYLE="width: 20%;"><img src="./image/deco.png" width="200" height="200"></td>
+                    <th style="padding-left: 50px; width: 20%">
+                        Username :
+                    </th>
+                    <td style="padding-left: 20px; border-bottom: 1px solid #E8A42A; width: 60%">
+                        shop name
+                    </td>
+                </tr>
+                <tr>
+                    <th style="padding-left: 50px; width: 20%">
+                        e-mail :
+                    </th>
+                    <td style="padding-left: 20px; border-bottom: 1px solid #E8A42A; width: 60%">
+                        email
+                    </td>
+                </tr>
+                <tr>
+                    <th style="padding-left: 50px; width: 20%">
+                        ชื่อร้าน :
+                    </th>
+                    <td style="padding-left: 20px; border-bottom: 1px solid #E8A42A; width: 60%">
+                        email
+                    </td>
+                </tr>
+                <tr>
+                    <th style="padding-left: 50px; width: 20%">
+                        ที่อยู่ :
+                    </th>
+                    <td style="padding-left: 20px; border-bottom: 1px solid #E8A42A; width: 60%">
+                        address
+                    </td>
+                </tr>
+                <tr>
+                    <th style="padding-top: 50px;" colspan="3">
+                        รายการอาหาร
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <div id="accordion">
+                            <div class="card">
+                                <div class="card-header" style="background-color: #EA4667">
+                                    <a class="card-link" data-toggle="collapse" href="#collapseOne" style="color: gold;">
+                                        <strong>อาหาร</strong>
+                                    </a>
+                                </div>
+                                <div id="collapseOne" class="collapse show" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <table style="width: 90%;">
+                                            <tr>
+                                                <th style="width: 80%;">รายการ</th>
+                                                <th style="width: 20%;">ราคา</th>
+                                            </tr>
+                                            <tr>
+                                                <td style="width: 70%; padding-left: 20px;">ข้าวผัดหมูกรอบ</td>
+                                                <td style="width: 30%;">45</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" style="background-color: #EA4667">
+                                    <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo" style="color: gold;">
+                                        <strong>เครื่องดื่ม</strong>
+
+                                    </a>
+                                </div>
+                                <div id="collapseTwo" class="collapse" data-parent="#accordion">
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" style="background-color: #EA4667">
+                                    <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree" style="color: gold;">
+                                        <strong>ขนม</strong>
+
+                                    </a>
+                                </div>
+                                <div id="collapseThree" class="collapse" data-parent="#accordion">
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 </div>
 
