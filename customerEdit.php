999fe83f71d15296768aa9bf0004344c23f38d02
Natthawut chipimol <natthawut_game2541@hotmail.com>
2020-01-21 15:12:03 +0700

    Game commit

--- customerEdit.php
+++ customerEdit.php
@@ -17,37 +17,40 @@
         <div class="col-8" >
             <div class="card mt-5" style="width: 100%;">
                 <div class="card-body">
-                    <h3 style="text-align: center">แก้ไขข้อมูลลูกค้า</h3>
+                    <h3 style=""><i class="far fa-edit"></i> แก้ไขข้อมูลลูกค้า</h3>
                     <form action="check.php?s=24" method="post">
-                        <table class="table mt-5">
+                        <table class="table mt-4" style="margin-bottom: 0px">
                         <tbody>
-                        <tr>
-                            <th scope="row">ชื่อ : </th>
-                            <td><input type="text" name="name" class="form-control" style="border: none" placeholder="Name" value="<?php echo $row["customer_name"]; ?>"></td>
+                            <tr>
+                                <th scope="row" style="border-top: none">ชื่อ : </th>
+                                <td style="border-top: none;border-bottom: solid #E8A42A;"><input type="text" name="name" class="form-control" style="border: none" placeholder="Name" value="<?php echo $row["customer_name"]; ?>"></td>
 
-                        </tr>
-                        <tr>
-                            <th scope="row">Username : </th>
-                            <td><input type="text" name="username" class="form-control" style="border: none" placeholder="Username" value="<?php echo $row["customer_username"]; ?>"></td>
+                            </tr>
+                            <tr>
+                                <th scope="row" style="border-top: none">Username : </th>
+                                <td style="border-top: none;border-bottom: solid #E8A42A;"><input type="text" name="username" class="form-control" style="border: none" placeholder="Username" value="<?php echo $row["customer_username"]; ?>"></td>
 
-                        </tr>
-                        <tr>
-                            <th scope="row">Password : </th>
-                            <td><input type="text" name="password" class="form-control" style="border: none" placeholder="Password" value="<?php echo $row["customer_password"]; ?>"></td>
+                            </tr>
+                            <tr>
+                                <th scope="row" style="border-top: none">Password : </th>
+                                <td style="border-top: none;border-bottom: solid #E8A42A;"><input type="text" name="password" class="form-control" style="border: none" placeholder="Password" value="<?php echo $row["customer_password"]; ?>"></td>
 
-                        </tr>
-                        <tr>
-                            <th scope="row">ที่อยู่ : </th>
-                            <td><input type="text" name="address" class="form-control" style="border: none" placeholder="Address" value="<?php echo $row["customer_address"]; ?>"></td>
+                            </tr>
+                            <tr>
+                                <th scope="row" style="border-top: none">ที่อยู่ : </th>
+                                <td style="border-top: none;border-bottom: solid #E8A42A;"><input type="text" name="address" class="form-control" style="border: none" placeholder="Address" value="<?php echo $row["customer_address"]; ?>"></td>
 
-                        </tr>
+                            </tr>
+                            <tr>
+                                <th scope="row" style="border-top: none">เบอร์โทร : </th>
+                                <td style="border-top: none;border-bottom: solid #E8A42A;"><input type="text" name="tel" class="form-control" style="border: none" PLACEHOLDER="Tel" value="<?php echo $row["customer_tel"]; ?>"></td>
+                            </tr>
                         <tr>
-                            <th scope="row">เบอร์โทร : </th>
-                            <td><input type="text" name="tel" class="form-control" style="border: none" PLACEHOLDER="Tel" value="<?php echo $row["customer_tel"]; ?>"></td>
+                            <td colspan="2"style="border-top: none; margin-top:">  <button type="submit" class="btn btn-outline-success btn-block" style="text-align: center">บันทึกข้อมูล</button></td>
                         </tr>
                         </tbody>
                     </table>
-                    <button type="submit" class="btn btn-outline-warning" style="text-align: center">บันทึกข้อมูล</button>
+
                     </form>
                 </div>
             </div>
